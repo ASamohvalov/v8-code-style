edfa9db8609c12a2bea0fc6e07e8959798c287f3
Artem Samohvalov <a.samohvalov@1c-izhtc.ru>
2026-09-03 09:58:14 +0400

    add standart number

--- bundles/com.e1c.v8codestyle.bsl/src/com/e1c/v8codestyle/bsl/check/PropertiesArrayModificationCheck.java
+++ bundles/com.e1c.v8codestyle.bsl/src/com/e1c/v8codestyle/bsl/check/PropertiesArrayModificationCheck.java
@@ -44,7 +44,7 @@ import com.e1c.g5.v8.dt.check.components.BasicCheck;
 import com.e1c.g5.v8.dt.check.components.ModuleTopObjectNameFilterExtension;
 import com.e1c.g5.v8.dt.check.settings.IssueSeverity;
 import com.e1c.g5.v8.dt.check.settings.IssueType;
-import com.e1c.v8codestyle.check.CommonSenseCheckExtension;
+import com.e1c.v8codestyle.check.StandardCheckExtension;
 import com.e1c.v8codestyle.internal.bsl.BslPlugin;
 
 /**
@@ -96,7 +96,7 @@ public class PropertiesArrayModificationCheck
             .severity(IssueSeverity.MINOR)
             .issueType(IssueType.CODE_STYLE)
             .extension(new ModuleTopObjectNameFilterExtension())
-            .extension(new CommonSenseCheckExtension(getCheckId(), BslPlugin.PLUGIN_ID))
+            .extension(new StandardCheckExtension(463, getCheckId(), BslPlugin.PLUGIN_ID))
             .module()
             .checkedObjectType(METHOD);
     }
